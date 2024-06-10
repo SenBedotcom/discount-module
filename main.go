952c123d193fdb1dd402b61b3b57f3ff5c2c5490
pongsakorn kitalreewan <pongsakornkit@gmail.com>
2024-06-10 11:11:03 +0700

    read json file and pre calculate total price from data

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"discount-module/model"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
 
 func main() {
-	fmt.Println("hi")
+	// Read json file
+
+	data, err := ioutil.ReadFile("filename")
+	if err != nil {
+		log.Fatalf("Error readig Json file: %v", err)
+	}
+
+	// Parse json data
+
+	var cart model.Cart
+	if err := json.Unmarshal(data, &cart); err != nil {
+		log.Fatal("Error parsing JSON data: %v", err)
+	}
+
+	// Calculate total price
+	totalPrice := 0.0
+	for _, item := range cart.Items {
+		totalPrice += item.Price
+	}
 }
